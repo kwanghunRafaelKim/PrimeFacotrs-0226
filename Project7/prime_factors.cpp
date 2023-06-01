98f75a1af967486984859aadac2b911844eeafde
kwanghun <lighthun@gmail.com>
2023-06-01 16:07:18 +0900

    refactoring if를 while로 변경

--- Project7/prime_factors.cpp
+++ Project7/prime_factors.cpp
@@ -10,8 +10,11 @@ public:
 		{
 			if (number == 4) 
 			{
-				result.push_back(2);
-				result.push_back(2);
+				while(number %2 == 0)
+				{
+					result.push_back(2);
+					number /= 2;
+				}
 			}
 			else
 			{
