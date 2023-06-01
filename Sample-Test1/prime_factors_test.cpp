51c3b3ad6fdc9d585238a3947006475feb6b9939
kwanghun <lighthun@gmail.com>
2023-06-01 15:51:22 +0900

    test.cpp Fixture 추가

--- Sample-Test1/prime_factors_test.cpp
+++ Sample-Test1/prime_factors_test.cpp
@@ -1,14 +1,19 @@
 #include "pch.h"
 #include "../Project7/prime_factors.cpp"
 
-using namespace std;
-TEST(PrimeFacotrs, Of1) {
+class PrimeFixture : public testing::Test
+{
+public:
 	PrimeFactor prime_factor;
-	vector<int> expected = {};
+	vector<int> expected;
+};
+
+using namespace std;
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
-TEST(PrimeFacotrs, Of2) {
-	PrimeFactor prime_factor;
-	vector<int> expected = {2};
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
